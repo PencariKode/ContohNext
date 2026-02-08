0668e04881d1c83ad14f8168aa8f5f5b04ea3035
BriantPanji <panjibriant@students.usu.ac.id>
2026-02-08 14:52:16 +0700

    Init edit

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function ContactPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            Ini halaman contact
+        </div>
+    )
+}
